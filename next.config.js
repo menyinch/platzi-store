ab1cd1abb234a718e191beb853640751a4bae29c
Menyin Chang <menyin_chang@hotmail.com>
2022-04-25 21:03:15 -0300

    Add: pwa config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,35 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    mode: 'production',
+    disable: false,
+  },
   reactStrictMode: true,
   images: {
     domains: ['placeimg.com'],
   },
-  // env: {
-  //   customKey: 'customValue',
-  // },
-  // basePath: '/dist',
-  // compress: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/hola',
-  //       destination: '/hello',
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
-};
+});
+
+// module.exports = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['placeimg.com'],
+//   },
+//   // env: {
+//   //   customKey: 'customValue',
+//   // },
+//   // basePath: '/dist',
+//   // compress: true,
+//   // async redirects() {
+//   //   return [
+//   //     {
+//   //       source: '/hola',
+//   //       destination: '/hello',
+//   //       permanent: true,
+//   //     },
+//   //   ];
+//   // },
+// };
